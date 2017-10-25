e8ac20acf336ecb74e3512fb6e1bc6d50ed3ee8d
Kobus Viljoen <kobusv@gmail.com>
2017-10-26 00:14:26 +0200

    improved suggestions

--- 2-dictionary/dictionary.py
+++ 2-dictionary/dictionary.py
@@ -1,19 +1,29 @@
 import json
-import difflib
 from difflib import get_close_matches
+
+def check_close_and_print(w, data):
+    possibleMatches = get_close_matches(w, data.keys(), cutoff=0.8)
+    if len(possibleMatches) > 0:
+        yn = input("Not found. Did you mean {0}? [y/n]".format(possibleMatches[0])).upper()
+        if yn == "Y":
+            for d in data[possibleMatches[0]]:
+                print('- {0}'.format(d))
+        elif yn == "N":
+            print("Unable to find word, possible matches: ")
+            for s in possibleMatches:
+                print(s)
+        else:
+            print("Invalid input.")
+    else:
+        print("Cannot find definition for the word. Please make sure it is correct.")
+
 
 def print_definition(w, data):
     if w in data:
         for d in data[w]:
             print('- {0}'.format(d))
     else:
-        possibleMatches = get_close_matches(w, data.keys())
-        if len(possibleMatches) > 0:
-            print("Unable to find word, possible matches: ")
-            for s in possibleMatches:
-                print(s)
-        else:
-            print("Cannot find definition for the word. Please make sure it is correct.")
+        check_close_and_print(w, data)
 
 
 data = json.load(open("data.json"))
