f4f470b7d19007e69c58964255ed9148e54bcf52
Kobus Viljoen <kobusv@gmail.com>
2017-10-30 19:32:21 +0200

    improved logic and cleaned code

--- 2-dictionary/dictionary.py
+++ 2-dictionary/dictionary.py
@@ -1,31 +1,35 @@
 import json
 from difflib import get_close_matches
 
-def check_close_and_print(w, data):
-    possibleMatches = get_close_matches(w, data.keys(), cutoff=0.8)
-    if len(possibleMatches) > 0:
-        yn = input("Not found. Did you mean {0}? [y/n]".format(possibleMatches[0])).upper()
+
+def print_definition(word_def):
+    if type(word_def) == list:
+        for wd in word_def:
+                print('- {0}'.format(wd))
+    else:
+        print(word_def)
+
+
+def get_close(w, data):
+    possible_matches = get_close_matches(w, data.keys(), cutoff=0.8)
+    if len(possible_matches) > 0:
+        yn = input("Not found. Did you mean {0}? [y/n]".format(possible_matches[0])).upper()
         if yn == "Y":
-            for d in data[possibleMatches[0]]:
-                print('- {0}'.format(d))
+            return data[possible_matches[0]]
         elif yn == "N":
-            print("Unable to find word, possible matches: ")
-            for s in possibleMatches:
-                print(s)
+            return "Unable to find definition."
         else:
-            print("Invalid input.")
-    else:
-        print("Cannot find definition for the word. Please make sure it is correct.")
-
-
-def print_definition(w, data):
-    if w in data:
-        for d in data[w]:
-            print('- {0}'.format(d))
-    else:
-        check_close_and_print(w, data)
+            return "Invalid input."
+    
+    return "Unable to find definition."
 
 
 data = json.load(open("data.json"))
-word = input("Enter word: ").lower()
-print_definition(word, data)
+word = input("Enter word: ")
+
+if word in data:
+    print_definition(data[word])
+elif word.lower() in data:
+    print_definition(data[word.lower()])
+else:
+    print_definition(get_close(word.lower(), data))
